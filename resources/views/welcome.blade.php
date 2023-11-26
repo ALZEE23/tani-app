5eac2f10a77fbfad63f1706405d887cd1fdce430
DESKTOP-SK1JDHR\ALZEE23 <muhamadalfarizial726@gmail.com>
2023-11-26 14:49:02 +0700

    tani-app

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,198 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en" class=" ">
+
+<head>
+  <!-- 
+         * @Package: Odis Mobile App 
+         * @Author: themepassion
+         * @Version: 1.0
+        -->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Odis Mobile App: </title>
+  <meta content="Odis Mobile App" name="description" />
+  <meta content="themepassion" name="author" />
+
+
+  <!-- App Icons -->
+  <link rel="apple-touch-icon" sizes="57x57" href="assets/images/icons/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="assets/images/icons/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="assets/images/icons/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/images/icons/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="assets/images/icons/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="assets/images/icons/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="assets/images/icons/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="assets/images/icons/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/images/icons/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="assets/images/icons/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="512x512" href="assets/images/icons/android-icon-512x512.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/images/icons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="assets/images/icons/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/images/icons/favicon-16x16.png">
+  <link rel="manifest" href="assets/images/icons/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="assets/images/icons/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+
+
+
+
+
+
+  <!-- CORE CSS FRAMEWORK - START -->
+  {{-- <link href="{{asset('css/preloader.css')}}" type="text/css" rel="stylesheet" media="screen" /> --}}
+
+  <link href="{{asset('modules/materialize/materialize.min.css')}}" type="text/css" rel="stylesheet" media="screen" />
+  <link href="{{asset('modules/fonts/mdi/appicon/appicon.css')}}" type="text/css" rel="stylesheet" media="screen" />
+  <link href="{{asset('modules/fonts/mdi/materialdesignicons.min.css')}}" type="text/css" rel="stylesheet" media="screen" />
+  <link href="{{asset('modules/perfect-scrollbar/perfect-scrollbar.css')}}" type="text/css" rel="stylesheet" media="screen" />
+
+
+  <!-- CORE CSS FRAMEWORK - END -->
+
+  <!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - START -->
+    <!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - END -->
+
+  <!-- CORE CSS TEMPLATE - START -->
+
+  
+    <link href="{{asset('css/style.css')}}" type="text/css" rel="stylesheet" media="screen" id="main-style" />
+  <!-- CORE CSS TEMPLATE - END -->
+  
+
+</head>
+<!-- END HEAD -->
+
+<!-- BEGIN BODY -->
+
+
+<body style="background-color: #66BB6A;" class=" menu-full  isfullscreen  html"  data-header="light" data-footer="light"  data-header_align="app"  data-menu_type="left" data-menu="light" data-menu_icons="on" data-footer_type="left" data-site_mode="light" data-footer_menu="show" data-footer_menu_style="light"  >
+    {{-- <div class="preloader-background">
+      <div class="preloader-wrapper">
+        <div class="loader">
+        </div>
+      </div>
+    </div> --}}
+  
+
+
+  <!-- SIDEBAR - START -->
+
+<!-- MAIN MENU - START -->
+
+
+
+<!-- MAIN MENU - END -->
+
+
+
+<!--  SIDEBAR - END --><!-- SIDEBAR - START -->
+
+<!-- MAIN MENU - START -->
+
+
+
+<!-- MAIN MENU - END -->
+
+
+
+<!--  SIDEBAR - END -->
+<div class="content-area">
+
+
+
+    <div class="row center" style="margin-top: 90px; margin-bottom: 90px;">
+      <h2>
+        DKP3
+      </h2>
+      <img src="assets/images/logo-login.png" alt="" height="170">
+      </div>
+
+  <div class="section pt0">
+    <div class="spacer"></div>
+
+
+
+
+
+    <div class="row ui-mediabox portfolio portfolio-masonry ">
+
+
+      <div class="col s4 grid-item">
+        <a class="img-wrap round" href="jt-login_petani.html" data-fancybox="images"
+          data-caption="Charming looking and young designs">
+          <img alt="image" class="" style="width: 100%;" src="{{asset('images/menu-petani.png')}}">
+        <h6 style="color:white" class="text-center">Petani</h6>
+
+        </a>
+        <div class="user-actions">
+        </div>
+      </div>
+
+      <div class="col s4 grid-item">
+        <a class="img-wrap round" href="jt-login_petani.html" data-fancybox="images"
+          data-caption="Create unreal combinations at your work">
+                    <img alt="image" class="" style="width: 100%;" src="{{asset('images/female.png')}}">
+        <h6 style="color:white" class="text-center">Petugas</h6>
+        </a>
+        <div class="user-actions">
+        </div>
+      </div>
+
+      <div class="col s4 grid-item">
+        <a class="img-wrap round" href="jt-login_petani.html" data-fancybox="images"
+          data-caption="You make your own looks stylish">
+          <img alt="image" class="" style="width: 100%;" src="{{asset('images/officer.png')}}">
+        <h6 style="color:white" class="text-center">Dinas</h6>
+        </a>
+        <div class="user-actions">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+</div><!--.content-area-->
+
+
+
+
+
+
+
+<script src="{{asset('assets/js/pwa.js')}}"></script>
+
+<!-- LOAD FILES AT PAGE END FOR FASTER LOADING -->
+
+<!-- CORE JS FRAMEWORK - START -->
+<script src="{{asset('modules/jquery/jquery-2.2.4.min.js')}}"></script>
+<script src="{{asset('modules/materialize/materialize.js')}}"></script>
+<script src="{{asset('modules/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
+<script src="{{asset('assets/js/variables.js')}}"></script>
+<!-- CORE JS FRAMEWORK - END -->
+
+
+<!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - START -->
+<script src="{{asset('assets/js/common.js')}}"></script><!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - END -->
+
+
+<!-- CORE TEMPLATE JS - START -->
+<script src="{{asset('modules/app/init.js')}}"></script>
+<script src="{{asset('modules/app/settings.js')}}"></script>
+
+<script src="{{asset('modules/app/scripts.js')}}"></script>
+
+<!-- END CORE TEMPLATE JS - END -->
+
+
+<script src="{{asset('assets/js/preloader.js')}}"></script>
+</body>
+
+</html>
